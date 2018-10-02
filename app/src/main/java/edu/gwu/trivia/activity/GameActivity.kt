9c14058eac1fc88c004c6a650bb92219689c5c31
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-10-02 08:44:31 -0400

    Fourth commit

--- app/src/main/java/edu/gwu/trivia/activity/GameActivity.kt
+++ app/src/main/java/edu/gwu/trivia/activity/GameActivity.kt
@@ -3,25 +3,47 @@ package edu.gwu.trivia.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
 import edu.gwu.trivia.BingImageSearchManager
+import edu.gwu.trivia.PersistenceManager
 import edu.gwu.trivia.R
 import edu.gwu.trivia.model.GameData
 import kotlinx.android.synthetic.main.activity_game.*
+import org.jetbrains.anko.toast
+import java.util.*
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(), BingImageSearchManager.ImageSearchCompletionListener {
     private val TAG = "GameActivity"
     private lateinit var gameData: GameData
     private lateinit var bingImageSearchManager: BingImageSearchManager
+    private lateinit var persistenceManager: PersistenceManager
 
     private var score = 0
     private var currentQuestionIndex = 0
     private var numWrong = 0
 
+    private var buttons = ArrayList<Button>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
+        setSupportActionBar(game_toolbar)
+
+        persistenceManager = PersistenceManager(this)
+
+        buttons.apply {
+            add(top_left_button)
+            add(top_right_button)
+            add(bottom_right_button)
+            add(bottom_left_button)
+        }
+
         bingImageSearchManager = BingImageSearchManager(this,image_view)
+        bingImageSearchManager.imageSearchCompletionListener = this
 
         gameData = intent.getParcelableExtra<GameData>(MenuActivity.GAME_DATA_KEY)
         Log.d(TAG, "we passed game data with ${gameData.questions.size} questions")
@@ -29,11 +51,25 @@ class GameActivity : AppCompatActivity() {
         nextTurn()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_game, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    fun skipButtonPressed(item: MenuItem) {
+        nextTurn()
+        item.isEnabled = false
+    }
+
     private fun nextTurn() {
         if(numWrong == 3 ) { //game over condition
-            //TODO
+            persistenceManager.saveScore(score)
+            finish()
         }
         else {
+            supportActionBar?.title = getString(R.string.score, score)
+
             currentQuestionIndex++
             currentQuestionIndex %= gameData.questions.size
 
@@ -43,8 +79,54 @@ class GameActivity : AppCompatActivity() {
 
             Log.d(TAG, "the correct answer is ${answer.answer}")
 
+
+            buttons.forEach {
+                it.isEnabled = false
+                it.text = ""
+            }
+
             bingImageSearchManager.searchImages(answer.answer)
         }
+    }
 
+    private fun displayAnswers() {
+
+        val answers = gameData.questions[currentQuestionIndex].wrongAnswers + gameData.questions[currentQuestionIndex].correctAnswer
+
+        Collections.shuffle(answers)
+
+        for(i in buttons.indices) {
+            val answer = answers[i]
+            val button = buttons[i]
+
+            button.apply {
+                text = answer.answer
+                tag = answer.correct
+                isEnabled = true
+            }
+        }
+    }
+
+    fun buttonPressed(view: View) {
+        val isCorrect = view.tag as Boolean
+
+        if(isCorrect) {
+            toast(R.string.correct)
+            score++
+        }
+        else {
+            toast(R.string.wrong)
+            numWrong++
+        }
+
+        nextTurn()
+    }
+
+    override fun imageLoaded() {
+        displayAnswers()
+    }
+
+    override fun imageNotLoaded() {
+        nextTurn()
     }
 }
